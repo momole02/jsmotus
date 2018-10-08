1bda6be68e0f3bc2e6e22de873fca61ec7566eed
Marc Arnaud A <Mac05@live.fr>
2018-10-08 18:58:26 +0000

    Prise en charge du dictionnaire

--- assets/game/panel.js
+++ assets/game/panel.js
@@ -24,6 +24,30 @@ class Panel{
 	}
 
 
+	generateWord(){
+		let dictLen = WORDS.length;
+		let choosed="";
+		do{
+			let rnd = Math.floor( Math.random()*(dictLen-1) );
+			choosed =  WORDS[rnd]; 
+		}while( choosed.length!=8 );
+		this.__toFind = choosed.toUpperCase();
+		
+		//console.log( this.__toFind);
+		let indexes = [];
+		for( let i=0;i<this.__toFind.length;++i )
+			indexes.push(i);
+
+		for( let i=0;i<5;++i ){ /* retirer 5 lettres */
+			let idx = Math.floor(Math.random()*indexes.length);
+			indexes = indexes.slice( 0 , idx ).concat( indexes.slice(idx+1,indexes.length) );
+		}
+		//console.log(indexes);
+		this.__masked="";
+		for( let i=0; i<this.__toFind.length;++i ){
+			this.__masked += ((indexes.indexOf(i)!==-1) ? this.__toFind[i]: '.');
+		}
+	}
 	/*
 		initialise le panel de jeu qui est une zone de dessin SVG
 	*/
@@ -32,15 +56,17 @@ class Panel{
 		let width = this.width;
 		let height = this.height;
 		let offset = this.offset;
+	
+		let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
 		this.__currentRow = 0;
 
+		this.generateWord();
 		$("#points").html(this.points);
-
 		$('#'+this.__marker).html('');
 		let element = SVG(this.__marker).size('100%',8*(offset+height));
-		let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-		this.element = element;
-
+		this.element = element;		
+		
 		for(let i=0;i<8;++i){
 			for( let j=0;j<8;++j ){
 				let x = i*(width+offset);
@@ -49,15 +75,6 @@ class Panel{
 					.rect(32, 32 )
 					.move( x, y)
 					.fill('#82D6FF');
-
-				/*element
-					.ellipse(32,32)
-					.move(x,y).fill('#FFF9B8');
-
-				element
-				.text(alphabet[(i+j*8)%26])
-				.font({size:30}).move( x+5,y );*/
-
 			}
 		}
 		this.writeWord( this.__masked );
@@ -133,7 +150,7 @@ class Panel{
 			}
 			if( win ){
 				$('#SVGDrawing').html('<h3 style="color:green">Vous avez gagné !</h3>');
-				setTimeout( ()=>{console.log('Hello');this.initialize()},1000);
+				setTimeout( ()=>{this.initialize()},1000);
 				this.points = this.points+15;
 				$("#points").html(this.points);
 
